9de5f412882ad89de4e9f54fd89e97c972bf41f0
s4ll <salmahr08@gmail.com>
2024-11-14 13:12:22 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'cashier_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Cashier App',
+      home:  CashierPage(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
